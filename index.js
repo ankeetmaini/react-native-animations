652635c27b0d21e694e13c19ff15c90447cddb11
Ankeet Maini <ankeet.maini@gmail.com>
2019-06-05 18:32:38 +0530

    Moves files to src/

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  */
 
 import { AppRegistry } from "react-native";
-import Rotate from "./Rotate";
-import Sequence from "./Sequence";
+import Rotate from "./src/Rotate";
+import Sequence from "./src/Sequence";
 import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => Sequence);
